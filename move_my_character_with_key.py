2e6f7a2b0724cce236a3ea0471261d8f1184f348
yunjinan@MYPC <anyoonjin0@gmail.com>
2024-09-30 19:44:38 +0900

    0930 키이를 누른 상태 동안 이동, 캐릭터 사이즈 조절

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,9 +7,7 @@ cha = load_image('character.png')
 
 
 def handle_events():
-    global running
-    global x
-    global y
+    global running ,dir1,dir2   #dir1 좌우 dir2 상하
 
     events = get_events()
     for event in events:
@@ -17,29 +15,42 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_LEFT:
-                x-=10
+                dir1-=1
             elif event.key == SDLK_RIGHT:
-                x+=10
+                dir1+=1
             elif event.key == SDLK_UP:
-                y+=10
+                dir2 +=1
             elif event.key == SDLK_DOWN:
-                y-=10
-        # fill here
+                dir2 -=1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_LEFT:
+                dir1 +=1
+            elif event.key == SDLK_RIGHT:
+                dir1-=1
+            elif event.key == SDLK_UP:
+                dir2 -=1
+            elif event.key == SDLK_DOWN:
+                dir2 +=1
 
 
 running = True
 x = 800 // 2
-y=600//2
+y= 600//6
+dir1 = 0
+dir2 = 0
 frame = 0
+len = 300//4
 
 while running:
     clear_canvas()
     grass.draw(400,300)
-    cha.clip_draw(frame*80,80,80,80,x,y)
+    cha.clip_draw(frame*len,len,len,len,x,y)
     update_canvas()
     handle_events()
     frame=(frame+1)%4
-    delay(0.07)
+    x+=dir1*5
+    y+=dir2*5
+    delay(0.08)
 
 
 close_canvas()
